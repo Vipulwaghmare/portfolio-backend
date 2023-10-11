37f4cd9c4511ad9c0c081a969b042124e984f07b
vipul <vipulwaghmare222@gmail.com>
2023-10-11 22:01:13 +0530

    swagger doc setup added

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ import express from "express";
 import dotEnv from "dotenv";
 dotEnv.config();
 import cors from "cors";
+import YAML from "yamljs";
 import mongoose from "mongoose";
 import helmet from "helmet";
+import swaggerUi from "swagger-ui-express";
 import connectToDatabase from "./config/database.js";
 import authRouter from "./routes/auth.routes.js";
 import { notFound, convertError } from "./middlewares/ErrorHandler.js";
+const swaggerDocument = YAML.load("./swagger/main.yaml");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 connectToDatabase();
 
