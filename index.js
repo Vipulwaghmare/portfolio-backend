7afcaa84f2c5e8b012f78a7a075fb5fb53f772a5
vipul <vipulwaghmare222@gmail.com>
2024-03-17 20:02:43 +0530

    added docker and rate-limiter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { createServer } from "http";
 import dotEnv from "dotenv";
 dotEnv.config();
+import { Server } from "socket.io";
+import requestIp from "request-ip";
 import cors from "cors";
 import YAML from "yamljs";
 import mongoose from "mongoose";
@@ -12,32 +15,47 @@ import productRouter from "./routes/product.routes.js";
 import todoRouter from "./routes/todo.routes.js";
 import { notFound, convertError } from "./middlewares/ErrorHandler.js";
 import userRouter from "./routes/user.routes.js";
+import rateLimiter from "./middlewares/rateLimiter.js";
 const swaggerDocument = YAML.load("./swagger/main.yaml");
 
 const app = express();
+const httpServer = createServer(app);
+connectToDatabase();
 
+// Socket IO
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*", // 'http://localhost:3000/'
+    credentials: true,
+  },
+});
+app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
+
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(requestIp.mw());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Rate Limiter
+app.use(rateLimiter);
 
-connectToDatabase();
-
+// Routes
 app.get("/", (req, res) => res.send("Welcome to API"));
 app.use("/api/v1", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1", productRouter);
 app.use("/api/v1/todo", todoRouter);
 
+// Error Handling
 app.use(convertError);
 app.use(notFound);
 
 const PORT = process.env.PORT;
 
 mongoose.connection.once("open", () => {
-  app.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
   });
 });
-
-export default app;
---
+++ middlewares/rateLimiter.js
@@ -0,0 +1,21 @@
+import { rateLimit } from "express-rate-limit";
+
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`,
+    );
+  },
+});
+
+export default rateLimiter;
