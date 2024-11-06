0d84bc8eec444ead3c274d80185b2c0d26a03374
vipul <vipulwaghmare222@gmail.com>
2024-11-06 20:53:16 +0530

    deleted build folder

--- build/utils/crypto.utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genRandomString = void 0;
-const node_crypto_1 = require("node:crypto");
-const genRandomString = () => (0, node_crypto_1.randomBytes)(32).toString("hex");
-exports.genRandomString = genRandomString;
-//# sourceMappingURL=crypto.utils.js.map
